7e03375141b0e23502fc9122048c96f759778c3a
White Coat Blue Tie <pharmacist05940@gmail.com>
2023-07-20 20:21:30 +0800

    final version

--- r.py
+++ r.py
@@ -3,9 +3,13 @@
 # 猜對的話 印出 " 終於猜對了"
 # 猜錯的話 要告訴他 比答案大/小
 
-import random 
+import random
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍結束值:')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1 # count = count + 1
